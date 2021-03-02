24ea49046d5003a39dca79eada6f35fc80e5db04
Bruno Martins Peçanha <brunotkf@hotmail.com>
2021-03-01 22:10:49 -0300

    First commit

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'webbarbershop-app'`, () => {
+  it(`should have as title 'WebBarberShop-app'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('webbarbershop-app');
+    expect(app.title).toEqual('WebBarberShop-app');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('webbarbershop-app app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('WebBarberShop-app app is running!');
   });
 });
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'webbarbershop-app';
+  title = 'WebBarberShop-app';
 }
