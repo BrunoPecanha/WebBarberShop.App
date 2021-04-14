2a1419f850ee1c7aec2439a8d7d2baa39309e320
Bruno Martins Peçanha <brunotkf@hotmail.com>
2021-04-14 14:46:00 -0300

    Inclui hora de inicio de atendimento

--- src/app/components/queue/queue.component.ts
+++ src/app/components/queue/queue.component.ts
@@ -7,79 +7,97 @@ import { Component, OnInit } from '@angular/core';
 })
 export class QueueComponent implements OnInit {
 
+  cliente: any;
   servicosClienteSelecionado: any;
 
   recuperarServicosCliente(posicao: number): void {
-      var cliente = this.clientes.find(x => x.posicao === posicao);
-      this.servicosClienteSelecionado = (cliente !== undefined && cliente.servicos !== undefined) ? cliente.servicos : ''       
+      var clienteLista = this.clientes.find(x => x.posicao === posicao);
+      this.cliente = clienteLista
+      this.servicosClienteSelecionado = (clienteLista !== undefined && clienteLista.servicos !== undefined) ? clienteLista.servicos : ''       
   }
 
   clientes = [
-     {
-        posicao: 1,
-        nome: 'Bruno',
+    {
+       posicao: 1,
+       nome: 'Bruno',
+       inicioem: '12:20',
+       sobrenome: 'Peçanha',
+       ematendomento: true,
+       barbeiro: 'Leonardo',
+       servicos: [
+         {
+           nome: 'Corte à máquina'
+         },
+         {
+           nome: 'Tintura'
+         }
+       ]
+    },
+    {
+     posicao: 2,
+     nome: 'Marvin',
+     inicioem: '14:17',
+     sobrenome: 'Daniel',
+     ematendomento: true,
+     barbeiro: 'Alisson',
+     servicos: [
+       {
+         nome: 'Corte à máquina'
+       },
+       {
+         nome: 'Disfarce'
+       }
+     ]
+   },  
+   {
+       posicao: 3,
+       nome: 'Anderson',
+       inicioem: '',
+       sobrenome: 'Honório',
+       ematendomento: false,
+       barbeiro: 'Leonardo',
+       servicos: [
+         {
+           nome: 'Corte à máquina'
+         }
+       ]
+   },
+   {
+     posicao: 4,
+     nome: 'Matheus',
+     inicioem: '14:17',
+     sobrenome: 'Henrique',
+     ematendomento: false,
+     barbeiro: 'Indiferente',
+     servicos: [
+       {
+         nome: 'Corte à máquina'
+       }
+     ]
+   },
+      {
+        posicao: 5,
+        nome: 'Peçanha',
         inicioem: '12:20',
-        sobrenome: 'Peçanha',
-        ematendomento: true,
-        barbeiro: 'Leonardo',
+        sobrenome: 'Bruno',
+        ematendomento: false,
+        barbeiro: 'Alisson',
         servicos: [
           {
             nome: 'Corte à máquina'
           },
           {
-            nome: 'Tintura'
+            nome: 'Tesoura'
           }
         ]
-     },
-     {
-      posicao: 2,
-      nome: 'Marvin',
-      inicioem: '12:20',
-      sobrenome: 'Daniel',
-      ematendomento: true,
-      barbeiro: 'Alisson',
-      servicos: [
-        {
-          nome: 'Corte à máquina'
-        },
-        {
-          nome: 'Disfarce'
-        }
-      ]
-    },  
-    {
-        posicao: 3,
-        nome: 'Anderson',
-        inicioem: '12:20',
-        sobrenome: 'Honório',
-        ematendomento: false,
-        barbeiro: 'Leonardo',
-        servicos: [
-          {
-            nome: 'Corte à máquina'
-          }
-        ]
-    },
-    {
-      posicao: 4,
-      nome: 'Matheus',
-      inicioem: '12:20',
-      sobrenome: 'Henrique',
+      },
+      {
+      posicao: 6,
+      nome: 'Débora',
+      inicioem: '',
+      sobrenome: 'Mary',
       ematendomento: false,
       barbeiro: 'Indiferente',
-      servicos: [
-        {
-          nome: 'Corte à máquina'
-        }
-      ]
-    },
-    {
-      posicao: 5,
-      nome: 'Peçanha',
-      inicioem: '12:20',
-      sobrenome: 'Bruno',
-      ematendomento: false,
-      barbeiro: 'Alisson',
       servicos: [
         {
           nome: 'Corte à máquina'
@@ -88,11 +106,43 @@ export class QueueComponent implements OnInit {
           nome: 'Tesoura'
         }
       ]
-   },
-   {
-    posicao: 5,
-    nome: 'Rose',
-    inicioem: '12:20',
+    },
+    {
+    posicao: 7,
+    nome: 'Tereza',
+    inicioem: '',
+    sobrenome: 'Mary',
+    ematendomento: false,
+    barbeiro: 'Indiferente',
+    servicos: [
+      {
+        nome: 'Sombrancelha'
+      },
+      {
+        nome: 'Tesoura'
+      }
+    ]
+    },
+    {
+    posicao: 8,
+    nome: 'Diana',
+    inicioem: '',
+    sobrenome: 'Mary',
+    ematendomento: false,
+    barbeiro: 'Indiferente',
+    servicos: [
+      {
+        nome: 'Corte à tesoura'
+      },
+      {
+        nome: 'Sombrancelha'
+      }
+    ]
+    },
+    {
+    posicao: 9,
+    nome: 'Thiago',
+    inicioem: '',
     sobrenome: 'Mary',
     ematendomento: false,
     barbeiro: 'Indiferente',
@@ -104,256 +154,202 @@ export class QueueComponent implements OnInit {
         nome: 'Tesoura'
       }
     ]
- },
- {
-  posicao: 5,
-  nome: 'Rose',
-  inicioem: '12:20',
-  sobrenome: 'Mary',
-  ematendomento: false,
-  barbeiro: 'Indiferente',
-  servicos: [
-    {
-      nome: 'Corte à máquina'
     },
     {
-      nome: 'Tesoura'
-    }
-  ]
-},
-{
-  posicao: 5,
-  nome: 'Rose',
-  inicioem: '12:20',
-  sobrenome: 'Mary',
-  ematendomento: false,
-  barbeiro: 'Indiferente',
-  servicos: [
-    {
-      nome: 'Corte à máquina'
+    posicao: 10,
+    nome: 'Rose',
+    inicioem: '',
+    sobrenome: 'Mary',
+    ematendomento: false,
+    barbeiro: 'Indiferente',
+    servicos: [
+      {
+        nome: 'Corte à máquina'
+      },
+      {
+        nome: 'Tesoura'
+      }
+    ]
     },
     {
-      nome: 'Tesoura'
-    }
-  ]
-},
-{
-  posicao: 5,
-  nome: 'Rose',
-  inicioem: '12:20',
-  sobrenome: 'Mary',
-  ematendomento: false,
-  barbeiro: 'Indiferente',
-  servicos: [
-    {
-      nome: 'Corte à máquina'
+    posicao: 11,
+    nome: 'Rose',
+    inicioem: '',
+    sobrenome: 'Mary',
+    ematendomento: false,
+    barbeiro: 'Indiferente',
+    servicos: [
+      {
+        nome: 'Corte à máquina'
+      },
+      {
+        nome: 'Tesoura'
+      }
+    ]
     },
     {
-      nome: 'Tesoura'
-    }
-  ]
-},
-{
-  posicao: 5,
-  nome: 'Rose',
-  inicioem: '12:20',
-  sobrenome: 'Mary',
-  ematendomento: false,
-  barbeiro: 'Indiferente',
-  servicos: [
-    {
-      nome: 'Corte à máquina'
+    posicao: 12,
+    nome: 'Rose',
+    inicioem: '',
+    sobrenome: 'Mary',
+    ematendomento: false,
+    barbeiro: 'Indiferente',
+    servicos: [
+      {
+        nome: 'Corte à máquina'
+      },
+      {
+        nome: 'Tesoura'
+      }
+    ]
     },
     {
-      nome: 'Tesoura'
-    }
-  ]
-},
-{
-  posicao: 5,
-  nome: 'Rose',
-  inicioem: '12:20',
-  sobrenome: 'Mary',
-  ematendomento: false,
-  barbeiro: 'Indiferente',
-  servicos: [
-    {
-      nome: 'Corte à máquina'
+    posicao: 13,
+    nome: 'Rose',
+    inicioem: '',
+    sobrenome: 'Mary',
+    ematendomento: false,
+    barbeiro: 'Indiferente',
+    servicos: [
+      {
+        nome: 'Corte à máquina'
+      },
+      {
+        nome: 'Tesoura'
+      }
+    ]
     },
     {
-      nome: 'Tesoura'
-    }
-  ]
-},
-{
-  posicao: 5,
-  nome: 'Rose',
-  inicioem: '12:20',
-  sobrenome: 'Mary',
-  ematendomento: false,
-  barbeiro: 'Indiferente',
-  servicos: [
-    {
-      nome: 'Corte à máquina'
+    posicao: 14,
+    nome: 'Rose',
+    inicioem: '',
+    sobrenome: 'Mary',
+    ematendomento: false,
+    barbeiro: 'Indiferente',
+    servicos: [
+      {
+        nome: 'Corte à máquina'
+      },
+      {
+        nome: 'Tesoura'
+      }
+    ]
     },
     {
-      nome: 'Tesoura'
-    }
-  ]
-},
-{
-  posicao: 5,
-  nome: 'Rose',
-  inicioem: '12:20',
-  sobrenome: 'Mary',
-  ematendomento: false,
-  barbeiro: 'Indiferente',
-  servicos: [
-    {
-      nome: 'Corte à máquina'
+    posicao: 15,
+    nome: 'Rose',
+    inicioem: '',
+    sobrenome: 'Mary',
+    ematendomento: false,
+    barbeiro: 'Indiferente',
+    servicos: [
+      {
+        nome: 'Corte à máquina'
+      },
+      {
+        nome: 'Tesoura'
+      }
+    ]
     },
     {
-      nome: 'Tesoura'
-    }
-  ]
-},
-{
-  posicao: 5,
-  nome: 'Rose',
-  inicioem: '12:20',
-  sobrenome: 'Mary',
-  ematendomento: false,
-  barbeiro: 'Indiferente',
-  servicos: [
-    {
-      nome: 'Corte à máquina'
+    posicao: 16,
+    nome: 'Rose',
+    inicioem: '',
+    sobrenome: 'Mary',
+    ematendomento: false,
+    barbeiro: 'Indiferente',
+    servicos: [
+      {
+        nome: 'Corte à máquina'
+      },
+      {
+        nome: 'Tesoura'
+      }
+    ]
     },
     {
-      nome: 'Tesoura'
-    }
-  ]
-},
-{
-  posicao: 5,
-  nome: 'Rose',
-  inicioem: '12:20',
-  sobrenome: 'Mary',
-  ematendomento: false,
-  barbeiro: 'Indiferente',
-  servicos: [
-    {
-      nome: 'Corte à máquina'
+    posicao: 17,
+    nome: 'Rose',
+    inicioem: '',
+    sobrenome: 'Mary',
+    ematendomento: false,
+    barbeiro: 'Indiferente',
+    servicos: [
+      {
+        nome: 'Corte à máquina'
+      },
+      {
+        nome: 'Tesoura'
+      }
+    ]
     },
     {
-      nome: 'Tesoura'
-    }
-  ]
-},
-{
-  posicao: 5,
-  nome: 'Rose',
-  inicioem: '12:20',
-  sobrenome: 'Mary',
-  ematendomento: false,
-  barbeiro: 'Indiferente',
-  servicos: [
-    {
-      nome: 'Corte à máquina'
+    posicao: 18,
+    nome: 'Rose',
+    inicioem: '',
+    sobrenome: 'Mary',
+    ematendomento: false,
+    barbeiro: 'Indiferente',
+    servicos: [
+      {
+        nome: 'Corte à máquina'
+      },
+      {
+        nome: 'Tesoura'
+      }
+    ]
     },
     {
-      nome: 'Tesoura'
-    }
-  ]
-},
-{
-  posicao: 5,
-  nome: 'Rose',
-  inicioem: '12:20',
-  sobrenome: 'Mary',
-  ematendomento: false,
-  barbeiro: 'Indiferente',
-  servicos: [
-    {
-      nome: 'Corte à máquina'
+    posicao: 19,
+    nome: 'Rose',
+    inicioem: '',
+    sobrenome: 'Mary',
+    ematendomento: false,
+    barbeiro: 'Indiferente',
+    servicos: [
+      {
+        nome: 'Corte à máquina'
+      },
+      {
+        nome: 'Tesoura'
+      }
+    ]
     },
     {
-      nome: 'Tesoura'
-    }
-  ]
-},
-{
-  posicao: 5,
-  nome: 'Rose',
-  inicioem: '12:20',
-  sobrenome: 'Mary',
-  ematendomento: false,
-  barbeiro: 'Indiferente',
-  servicos: [
-    {
-      nome: 'Corte à máquina'
+    posicao: 20,
+    nome: 'Rose',
+    inicioem: '',
+    ematendomento: false,
+    sobrenome: 'Mary',
+    barbeiro: 'Indiferente',
+    servicos: [
+      {
+        nome: 'Corte à máquina'
+      },
+      {
+        nome: 'Tesoura'
+      }
+    ]
     },
     {
-      nome: 'Tesoura'
-    }
-  ]
-},
-{
-  posicao: 5,
-  nome: 'Rose',
-  inicioem: '12:20',
-  sobrenome: 'Mary',
-  ematendomento: false,
-  barbeiro: 'Indiferente',
-  servicos: [
-    {
-      nome: 'Corte à máquina'
-    },
-    {
-      nome: 'Tesoura'
-    }
-  ]
-},
-{
-  posicao: 5,
-  nome: 'Rose',
-  inicioem: '12:20',
-  ematendomento: false,
-  sobrenome: 'Mary',
-  barbeiro: 'Indiferente',
-  servicos: [
-    {
-      nome: 'Corte à máquina'
-    },
-    {
-      nome: 'Tesoura'
-    }
-  ]
-},
-{
-  posicao: 5,
-  nome: 'Rose',
-  inicioem: '12:20',
-  sobrenome: 'Mary',
-  ematendomento: false,
-  barbeiro: 'Indiferente',
-  servicos: [
-    {
-      nome: 'Corte à máquina'
-    },
-    {
-      nome: 'Tesoura'
-    }
-  ]
-},
+    posicao: 21,
+    nome: 'Rafael',
+    inicioem: '',
+    sobrenome: 'Matheus',
+    ematendomento: false,
+    barbeiro: 'Indiferente',
+    servicos: [
+      {
+        nome: 'Corte à máquina'
+      },
+      {
+        nome: 'Tesoura'
+      }
+ ]
+}]
 
-
-
-]
- 
-
-  constructor() { }
+  constructor() { }  
 
   ngOnInit(): void {
   }
-
 }
